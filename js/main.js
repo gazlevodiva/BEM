7a4d6d25a20a97928bcc725cbcc71a32cfb674fb
Alexey <almarka03@gmail.com>
2020-07-20 20:26:37 +0300

    refactor shit code, fix card generate, and blablabla...)

--- js/main.js
+++ js/main.js
@@ -2,23 +2,35 @@ var MARKET = [{ 'name':'Украина',
                 'specs':{2020:5, 2021:6, 2022:8, 2023:15},
                     },
               { 'name':'Россия', 
-                'specs':{2020:5, 2021:6, 2022:8, 2023:15},
+                'specs':{2020:12, 2021:16, 2022:17, 2023:18},
                     },
             ];
 
+var STAFF = {2020:50, 2021:55, 2022:65, 2023:5, 2024: 10};
 
 
-var STAFF = {2020:50, 2021:55, 2022:65};
-var YEAR = 2020;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var START_YEAR = 2020;
 var CURRENT_YEAR = 2020;
-
 
 window.onload = function(){
     for (mar_id in MARKET){
         add_market_card(mar_id);
     }
 }
-
 
 
 function add_market_card(market_id){
@@ -34,11 +46,12 @@ function add_market_card(market_id){
 
     elem.innerHTML = '<div class="card-header">'+
                      '<h4 class="my-0 font-weight-normal">'+market_name+'</h4>'+
-                     '</div><div class="card-body"><h1 class="card-title pricing-card-title">0 спец '+
-                     '<small class="text-muted" id="market_year">/ '+YEAR+'</small>'+
-                     '</h1><ul class="list-unstyled mt-3 mb-4"><li>...</li></ul>'+
+                     '</div><div class="card-body">'+
+                     '<h1 class="card-title pricing-card-title" id="specs_market_'+market_id+'">'+MARKET[market_id].specs[CURRENT_YEAR]+' спец '+
+                     '<small class="text-muted" id="market_year_'+market_id+'">/ '+CURRENT_YEAR+'</small></h1>'+
+                     '<ul class="list-unstyled mt-3 mb-4"><li>...</li></ul>'+
                      '<button type="button" class="btn btn-lg btn-block btn-outline-secondary" data-toggle="modal"'+
-                     'data-target="#modal'+market_id+'" onclick="market_settings_view('+market_id+')">Настройки</button></div>';
+                     'data-target="#modal'+market_id+'">Настройки</button></div>';
     $( elem ).insertBefore( "#add_market_btn" );
     event.preventDefault();   	
     add_modal(market_id, market_name);
@@ -53,10 +66,10 @@ function add_modal(market_id, market_name){
                      '<h5 class="modal-title" id="modal'+market_id+'">Настройки</h5>'+
                      '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'+
                      '<span aria-hidden="true">&times;</span></button></div><div class="modal-body">'+
-                     '<h4 class="mb-3" id="modal_market_name"'+market_name+'</h4>'+
+                     '<h4 class="mb-3" id="modal_market_name">'+market_name+'</h4>'+
                      '<form name="market_settings" onsubmit="check_market_settings()"><div class="mb-3">'+
                      '<label for="username">Название рынка</label><div class="input-group">'+
-                     '<input type="text" class="form-control" id="market" placeholder="Введите название" required="">'+
+                     '<input type="text" class="form-control" id="market" placeholder="Введите название" required="" value="'+market_name+'">'+
                      '</div></div><div class="mb-3" id="header-elem"><label>2020</label><input type="text" class="form-control" id="" required></div>'+
                      '<button class="btn btn-outline-secondary" onclick="adder()">Добавить дату</button>'+
                      '<button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>'+
@@ -66,38 +79,23 @@ function add_modal(market_id, market_name){
     event.preventDefault();
 }
 
+function getlast(dict){
+    if (dict[CURRENT_YEAR] === undefined) {
+        current_value = Object.values(dict)[Object.values(dict).length-1];
+    }else{
+        current_value = dict[CURRENT_YEAR];
+    }
+    return current_value;
+}
+
 
 var i = 1;      
 function adder(){
     var elem = document.createElement("div");
     elem.setAttribute("class", "mb-3");
-    elem.innerHTML = '<label>'+(YEAR+i)+'</label>'+
+    elem.innerHTML = '<label>'+(CURRENT_YEAR+i)+'</label>'+
     '<input type="text" class="form-control" id="" required>';
     $( elem ).insertAfter( "#header-elem" );
     event.preventDefault();
     i+=1;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-function check_market_settings(){
-    // window.alert('OLOLO')
-}
-
-
-function market_settings_view(params){
-    var modal_market_name = document.getElementById("modal_market_name");
-    modal_market_name.innerHTML = MARKET[params].name;
-}
-
-
